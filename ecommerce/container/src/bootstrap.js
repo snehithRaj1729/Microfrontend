aed2b0e7a1e29a13c9ab61452159ec76834af440
snehithRaj1729 <snehithraj1729@gmail.com>
2024-05-18 15:13:22 +0530

    Integrated both products,cart child MFEs to parent MFE which is container

--- ecommerce/container/src/bootstrap.js
+++ ecommerce/container/src/bootstrap.js
@@ -1,5 +1,6 @@
 import 'products/ProductsIndex'; //webpack sees that we are trying to import the module that begins with the word products
 // If webpack cannot find a products dependency in nodemodulse then it will search for products in webpack.config.js mainly in remotes  section
+import 'cart/CartShow';
 
 console.log("we are inside the container");
  
--- ecommerce/container/webpack.config.js
+++ ecommerce/container/webpack.config.js
@@ -10,8 +10,9 @@ module.exports = {
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
-                products: 'products@http://localhost:8081/remoteEntry.js'
+                products: 'products@http://localhost:8081/remoteEntry.js',
 // products in the key before the url must be same as the key value of name in products webpack.config.js
+                cart: 'cart@http://localhost:8082/remoteEntry.js'
             },
         }),
         new HtmlWebpackPlugin({
